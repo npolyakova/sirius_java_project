fc4d29e15cdb3b573bb9c1e008d7ac8a31271e9a
mikhailrovnyagin <rovnyagin@gmail.com>
2025-03-12 00:55:25 +0300

    spring core init

---
+++ src/main/java/ru/hpclab/bd/module1/bean/FirstBean.java
@@ -0,0 +1,12 @@
+package ru.hpclab.bd.module1.bean;
+
+public class FirstBean {
+    private final String greeting = "Hello world!";
+
+    public FirstBean() {
+    }
+
+    public String getGreeting() {
+        return greeting;
+    }
+}
---
+++ src/main/java/ru/hpclab/bd/module1/bean/SecondBean.java
@@ -0,0 +1,12 @@
+package ru.hpclab.bd.module1.bean;
+
+public class SecondBean extends FirstBean {
+    private final String greeting = "World was investigated";
+
+    public SecondBean() {
+    }
+
+    public String getGreeting() {
+        return greeting;
+    }
+}
